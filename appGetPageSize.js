84a5f6e9643067e58bc7af61f8d8cb6c2d4c22b5
force4u <clubsmoky@gmail.com>
2015-06-21 22:31:28 +0900

    Update appGetPageSize.js

--- appGetPageSize.js
+++ appGetPageSize.js
@@ -1,6 +1,6 @@
 //
 //appGetPageSize.js
-// 20150621 rev02
+// 20150621 rev04
 //
 //ダウンロード後文字コードをUTF16に変更してからインストールする
 //
@@ -25,18 +25,18 @@ function GetPageSizeAlert(){
 	
 	//各サイズをリストで取得
 	var MediaBoxSize = this.getPageBox("Media", nPage);
-	var CropBoxSize = this.getPageBox("Crop", nPage);
-	var TrimBoxSize = this.getPageBox("Trim", nPage);
-	var ArtBoxSize = this.getPageBox("Art", nPage);
 	var BleedBoxSize = this.getPageBox("Bleed", nPage);
+	var TrimBoxSize = this.getPageBox("Trim", nPage);
+	var CropBoxSize = this.getPageBox("Crop", nPage);
+	var ArtBoxSize = this.getPageBox("Art", nPage);
 	var BoundingBoxBoxSize = this.getPageBox("BBox", nPage);
 
 //ログ
 console.println('Media: ' + MediaBoxSize);
-console.println('Crop: ' + CropBoxSize);
-console.println('Trim: ' + TrimBoxSize);
-console.println('Art: ' + ArtBoxSize);
 console.println('Bleed: ' + BleedBoxSize);
+console.println('Trim: ' + TrimBoxSize);
+console.println('Crop: ' + CropBoxSize);
+console.println('Art: ' + ArtBoxSize);
 console.println('BBox: ' + BoundingBoxBoxSize);
 console.println('\n\n');
 
@@ -44,23 +44,23 @@ console.println('\n\n');
 //リストから幅と高さを計算
 	var MediaBoxSizeWidth = MediaBoxSize[2] - MediaBoxSize[0];
 	var MediaBoxSizeHeight = MediaBoxSize[1] - MediaBoxSize[3];
-	var CropBoxSizeWidth = CropBoxSize[2] - CropBoxSize[0];
-	var CropBoxSizeHeight = CropBoxSize[1] - CropBoxSize[3];
-	var TrimBoxSizeWidth = TrimBoxSize[2] - TrimBoxSize[0];
-	var TrimBoxSizeHeight = TrimBoxSize[1] - TrimBoxSize[3];
-	var ArtBoxSizeWidth = ArtBoxSize[2] - ArtBoxSize[0];
-	var ArtBoxSizeHeight = ArtBoxSize[1] - ArtBoxSize[3];
 	var BleedBoxSizeWidth = BleedBoxSize[2] - BleedBoxSize[0];
 	var BleedBoxSizeHeight = BleedBoxSize[1] - BleedBoxSize[3];
+	var TrimBoxSizeWidth = TrimBoxSize[2] - TrimBoxSize[0];
+	var TrimBoxSizeHeight = TrimBoxSize[1] - TrimBoxSize[3];
+	var CropBoxSizeWidth = CropBoxSize[2] - CropBoxSize[0];
+	var CropBoxSizeHeight = CropBoxSize[1] - CropBoxSize[3];
+	var ArtBoxSizeWidth = ArtBoxSize[2] - ArtBoxSize[0];
+	var ArtBoxSizeHeight = ArtBoxSize[1] - ArtBoxSize[3];
 	var BoundingBoxWidth = BoundingBoxBoxSize[2] - BoundingBoxBoxSize[0];
 	var BoundingBoxHeight = BoundingBoxBoxSize[1] - BoundingBoxBoxSize[3];
 
 //ログ
 console.println(MediaBoxSizeWidth + " " + MediaBoxSizeHeight);
-console.println(CropBoxSizeWidth + " " + CropBoxSizeHeight);
-console.println(TrimBoxSizeWidth + " " + TrimBoxSizeHeight);
-console.println(ArtBoxSizeWidth + " " + ArtBoxSizeHeight);
 console.println(BleedBoxSizeWidth + " " + BleedBoxSizeHeight);
+console.println(TrimBoxSizeWidth + " " + TrimBoxSizeHeight);
+console.println(CropBoxSizeWidth + " " + CropBoxSizeHeight);
+console.println(ArtBoxSizeWidth + " " + ArtBoxSizeHeight);
 console.println(BoundingBoxWidth + " " + BoundingBoxHeight);
 
 //ピクセル情報からmmに変換
@@ -77,55 +77,53 @@ console.println(BoundingBoxWidth + " " + BoundingBoxHeight);
 	BoundingWidth = (Math.round(BoundingBoxWidth*px2mm*10))/10;
 	BoundingHeight = (Math.round(BoundingBoxHeight*px2mm*10))/10;
 
-
+//サイズ警告用
+BleedSizeAlert = (" ");
+if(BleedSizeWidth > MediaSizeWidth){
+	BleedSizeAlert = ("【情報】塗足しブリードサイズが\n用紙サイズを超えている可能性があります(w-Bleed)\n-------\n");
+}
+if(BleedSizeHeight > MediaSizeHeight){
+	BleedSizeAlert = ("【情報】塗足しブリードサイズが\n用紙サイズを超えている可能性があります(h-Bleed)\n-------\n");
+}
 //サイズ警告用
 ArtSizeAlert = (" ");
-
 if(ArtSizeWidth > CropSizeWidth){
 	ArtSizeAlert = ("【情報】表示サイズの外に\nオブジェクトがある可能性があります(w-Art)\n-------\n");
 }
 if(ArtSizeHeight > CropSizeHeight){
 	ArtSizeAlert = ("【情報】表示サイズの外に\nオブジェクトがある可能性があります(h-Art)\n-------\n");
 }
-
+//サイズ警告用
 TrimSizeAlert = (" ");
-
 if(TrimSizeWidth > BleedSizeWidth){
-	TrimSizeAlert = ("【情報】塗りたしサイズより\n仕上がりが大きい可能性があります(w-Trim)\n-------\n");
+	TrimSizeAlert = ("【情報】塗足しサイズより\n仕上がりが大きい可能性があります(w-Trim)\n-------\n");
 }
 if(TrimSizeHeight > BleedSizeHeight){
-	TrimSizeAlert = ("【情報】塗りたしサイズより\n仕上がりが大きい可能性があります(h-Trim)\n-------\n");
+	TrimSizeAlert = ("【情報】塗足しサイズより\n仕上がりが大きい可能性があります(h-Trim)\n-------\n");
 }
-
-console.println("\n" + MediaBoxSize[0] + "\n");
-
-
+//サイズ警告用
 MediaZeroAlert = (" ");
-
-
-
 if( 0 > MediaBoxSize[0] ){
 	MediaZeroAlert = ("-------\n【警告！】メディアサイズに負の値があります(m-Zero)\n");
 }
-
-
 
 nPage = nPage + 1 ;
 
 
 //アラートを表示
 	app.alert(
-	TrimSizeAlert + " " + ArtSizeAlert + "\n" + "MediaSize\nメディアサイズ・用紙サイズ\n（PS 記述上の出力メディアの大きさ）:\n Width: " + 
-	MediaSizeWidth +  "mm  Height : " + MediaSizeHeight + "mm\n\n" + 
-			"BleedSize\nブリード・裁ち落としサイズ\n（塗り足しを加えたページの大きさ）:\n  Width: " + 
+	BleedSizeAlert + " " + TrimSizeAlert + " " + ArtSizeAlert + "\n" + 
+			"MediaSize\nメディア・サイズ・用紙サイズ\n（PS 記述上の出力メディアの大きさ）:\n Width: " + 
+			MediaSizeWidth +  "mm  Height : " + MediaSizeHeight + "mm\n\n" + 
+			"BleedSize\nブリード・裁ち落としサイズ\n（塗足しを加えたページの大きさ）:\n  Width: " + 
 			BleedSizeWidth +  "mm  Height : " + BleedSizeHeight + "mm\n\n" + 
-			"TrimSize\nページサイズ・仕上がりサイズ\n（実際の最終仕上りページの大きさ）:\n  Width: " + 
+			"TrimSize\nページ・サイズ・仕上がりサイズ\n（実際の最終仕上りページの大きさ）:\n  Width: " + 
 			TrimSizeWidth +  "mm  Height : " + TrimSizeHeight + "mm\n\n" + 
-			"CropSize\nトリミングサイズ・表示サイズ\n（画面に表示可能な大きさ）:\n  Width: " + 
+			"CropSize\nトリミング・サイズ・表示サイズ\n（画面に表示可能な大きさ）:\n  Width: " + 
 			CropSizeWidth +  "mm  Height : " + CropSizeHeight + "mm\n\n" + 
-			"ArtSize\nアート・オブジェクトサイズ\n（Cropトリミングサイズと同一がデフォルト）:\n  Width: " + 
+			"ArtSize\nアート・サイズ\n（Cropトリミングサイズと同一がデフォルト）:\n  Width: " + 
 			ArtSizeWidth +  "mm  Height : " + ArtSizeHeight + "mm\n\n" + 
-			"BoundingBoxSize\nバウンディングボックス\n（オブジェクトの境界線ボックスサイズ）:\n  Width: " + 
+			"BoundingBoxSize（Read-only）\nバウンディングボックス\n（オブジェクトの境界線ボックスサイズ）:\n  Width: " + 
 			BoundingWidth +  "mm  Height : " + BoundingHeight + "mm\n\n" + nPage + "ページの情報" +
 			"\n\n" + MediaZeroAlert + " ");
 }
